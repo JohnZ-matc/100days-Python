b04de985df9b95b8e2cfefe10b9745883cf989a3
John_Zimmerman <jbz.matc@gmail.com>
2023-07-29 00:29:04 -0500

    reorganized dir and add day1 project

---
+++ day1/main.py
@@ -0,0 +1,13 @@
+# Band Name generator program
+
+# print the greeting to the console
+print('Welcome to the Band Name Generator!\n')
+
+# use the input function to allow the user to type the answer to question 1
+a = input('What city did you grow up in?\n')
+
+# do the same thing in this step for question 2
+b = input('What was the name of your first pet?\n')
+
+# The solution should output a the solution
+print('Your band name is, ' + a + " " + b)
