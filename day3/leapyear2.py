b311871b7f2d17c5e5808b35b7f0134b57ae89c9
John_Zimmerman <jbz.matc@gmail.com>
2023-08-07 19:22:43 -0500

    add day3/leapyear,day3leapyear2.py and mod day3/test.py

---
+++ day3/leapyear2.py
@@ -0,0 +1,29 @@
+# Leap Year Calculator Program!
+
+# String Variables
+
+welcome = str("This is the Leap Year Calculator Program!\n")
+prgmDesc = str("How it works: Enter a year, and the program will tell you if that year was a leap year.\n")
+
+# Integer Variables
+
+
+# Program
+
+print(welcome + prgmDesc)
+
+y = round(float(input("Enter a year: \n")), 2)
+
+leapy1 = (y % 4 == 0)
+leapy2 = (y % 100 == 0)
+leapy3 = (y % 400 == 0)
+
+if leapy1:
+    if leapy2:
+        print("Not leap")
+    elif leapy3:
+        print("Leap")
+    else:
+        print("leap")
+else:
+    print("Not leap")
